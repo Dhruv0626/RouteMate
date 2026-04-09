c01de000463c3eb8a8c15035d888ef03f4a4835f
Dhruv0626 <dhruvbhavsar6205@gmail.com>
2026-04-09 18:44:03 +0530

    app email changed

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -4,9 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-mail.outlook.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
